6c41deaff4112d180dfed6dc1af3add75bdf65ee
krenova <krenova@gmx.com>
2026-03-29 22:54:44 +0800

    omitted commit for phase 1

--- ama_safeguards/__init__.py
+++ ama_safeguards/__init__.py
@@ -1,5 +1,21 @@
 from ama_safeguards.circuit_breaker import CircuitBreaker
 from ama_safeguards.exit_gate import ExitGate, ExitGateState
 from ama_safeguards.rate_limiter import RateLimiter, RateLimitError
+from ama_safeguards.status_writer import (
+    write_status,
+    register_worker,
+    deregister_worker,
+    get_active_workers,
+)
 
-__all__ = ["CircuitBreaker", "ExitGate", "ExitGateState", "RateLimiter", "RateLimitError"]
+__all__ = [
+    "CircuitBreaker",
+    "ExitGate",
+    "ExitGateState",
+    "RateLimiter",
+    "RateLimitError",
+    "write_status",
+    "register_worker",
+    "deregister_worker",
+    "get_active_workers",
+]
